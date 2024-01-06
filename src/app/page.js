ea6c11d6eed52c61ba2e77fdc668b36497e3b5ee
Divya Raj <divyaraj7612@gmail.com>
2024-01-06 22:20:29 +0530

    modified pages.jsx

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,25 @@
-import NextGenCards from "../components/nextGenCards"
-import NewCardSoln from "../components/newCardSoln"
-import Review from "../components/review"
-import Navbar from "../components/navbar"
-import ElevateCard from "../components/elevateCard"
-import Footer from "../components/footer"
-
+import NextGenCards from "../components/nextGenCards";
+import NewCardSoln from "../components/newCardSoln";
+import Review from "../components/review";
+import Navbar from "../components/navbar";
+import ElevateCard from "../components/elevateCard";
+import Footer from "../components/footer";
+import { FeatureDescription,FeatureImages } from "../components/elevateCard";
 
 export default function Home() {
   return (
     <main className="mainContainer">
-      <Navbar/>
+      <Navbar />
       <NextGenCards />
-      <ElevateCard />
+      <ElevateCard>
+        <FeatureDescription />
+      </ElevateCard>
+      <ElevateCard>
+        <FeatureImages />
+      </ElevateCard>
       <Review />
       <NewCardSoln />
       <Footer />
     </main>
-  )
+  );
 }
