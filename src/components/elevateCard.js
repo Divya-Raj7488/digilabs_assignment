306136d59127dbafc4f8a1962d5059ea4fa3d445
Divya Raj <divyaraj7612@gmail.com>
2024-01-06 22:19:42 +0530

    created components for elevating card and next gen card solution

--- src/components/elevateCard.js
+++ src/components/elevateCard.js
@@ -1,9 +1,81 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import purpleArrow from "../../assets/purpleArrow.svg";
+import globe from "../../assets/globe.svg";
+import key from "../../assets/key.svg";
+import lock from "../../assets/lock.svg";
+import blackDisplay from "../../assets/blackDisplay.svg"
+import cardImg from "../../assets/cardImg.svg"
 
-export default function ElevateCard() {
+export default function ElevateCard({ children }) {
   return (
-    <div>
-      
+    <div className="elevateCard">
+      <div className="textContainer">
+        <h2 className="elevateCardsHeading">
+          Elevating Card Programs with Cutting-Edge Technology
+        </h2>
+        <h5 className="elevateCardsdescription">
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua.
+        </h5>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            height: "1rem",
+          }}
+        >
+          <div className="compareFeatures">Compare all Pro features</div>
+          <Image src={purpleArrow} width={20} height={20} alt="arrow" />
+        </div>
+        {children}
+      </div>
     </div>
-  )
+  );
 }
+
+const FeatureDescription = () => {
+  return (
+    <div className="featuresDiv">
+      <div className="features">
+        <Image src={globe} width={24} height={24} alt="globe" />
+        <div className="featureName">Globally Accepted</div>
+        <div className="featureDescription">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod{" "}
+        </div>
+      </div>
+      <div className="features">
+        <Image src={key} width={24} height={24} alt="key" />
+        <div className="featureName">Biometric Integrated</div>
+        <div className="featureDescription">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod{" "}
+        </div>
+      </div>
+      <div className="features">
+        <Image src={lock} width={24} height={24} alt="lock" />
+        <div className="featureName">Fully Secured</div>
+        <div className="featureDescription">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod{" "}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const FeatureImages = () => {
+  return (
+    <div className="fImgContainer">
+      <div className="featureImg">
+        <Image src={blackDisplay} width={394} height={326} />
+      </div>
+      <div className="featureImg">
+      <Image src={cardImg} width={394} height={326} />
+      </div>
+    </div>
+  );
+};
+export { FeatureDescription, FeatureImages };
--- src/components/nextGenCards.js
+++ src/components/nextGenCards.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import arrowRight from "../../assets/arrowRight.svg";
+import NuxtLogo from "../../assets/NuxtLogo.svg"
+import miro from "../../assets/miro.svg"
+import lotiefLogo from "../../assets/lotiefLogo.svg"
+import wiselogo from "../../assets/wiselogo.svg"
+import dribble from "../../assets/dribble.svg"
+import kinstaLogo from "../../assets/kinstaLogo.svg"
+import angelistlogo from "../../assets/angelistlogo.svg"
+import Bihancelogo from "../../assets/Bihancelogo.svg"
 
 export default function NextGenCards() {
   return (
-    <div>
-      
+    <div className="nextGenCard">
+      <div className="HeadingAndDescription">
+        <div className="seamlessExperience">Seamless experience</div>
+        <h1 className="cardHeading">
+          Unleashing the Next Generation of Card Solutions
+        </h1>
+        <h5 className="Description">
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+        </h5>
+      </div>
+      <button className="unlockBtn">
+        <div> Unlock your card</div>
+        <Image src={arrowRight} width={20} height={20} alt="arrow" />
+      </button>
+      <div className="requirement">*No credit cards required.</div>
+      <div className="logoCloud">
+        <Image src={NuxtLogo} height={32} width={112} alt="logo" />
+        <Image src={miro} height={32} width={84} alt="logo" />
+        <Image src={lotiefLogo} height={32} width={140} alt="logo" />
+        <Image src={wiselogo} height={32} width={122} alt="logo" />
+        <Image src={dribble} height={32} width={114} alt="logo" />
+        <Image src={kinstaLogo} height={32} width={136} alt="logo" />
+        <Image src={angelistlogo} height={32} width={118} alt="logo" />
+        <Image src={Bihancelogo} height={32} width={148} alt="logo" />
+      </div>
     </div>
-  )
+  );
 }
